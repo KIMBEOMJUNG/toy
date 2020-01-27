bdcfcf63a530c48db5b70ca01f389ec64792d5a5
KIMBEOMJUNG <rla5451@naver.com>
2020-01-27 19:35:52 +0900

    셋팅

---
+++ toy/src/main/java/egovframework/example/sample/web/mainController.java
@@ -0,0 +1,29 @@
+package egovframework.example.sample.web;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+import egovframework.example.sample.service.impl.SampleDAO;
+
+@Controller
+public class mainController {
+	@Resource(name = "sampleDAO")
+	private SampleDAO sampleDAO;
+	@Resource(name= "multipartResolver")
+	CommonsMultipartResolver multipartResolver;
+	
+	@RequestMapping(value="/main/main.do")
+	public String main(HttpServletRequest request, ModelMap model) throws Exception
+	{
+		return "main/main";
+	}
+
+	
+}
